fae6b46fe9a0124c22b341a71fc5deee3bd73f1c
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-31 16:06:35 +0100

    add hit points to players

--- app.rb
+++ app.rb
@@ -11,12 +11,16 @@ enable :sessions
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
+    session[:player_1_points] = 0
+    session[:player_2_points] = 0
     redirect '/play'
   end
 
   get '/play' do
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
+    @player_1_points = session[:player_1_points]
+    @player_2_points = session[:player_2_points]
     erb(:play)
   end
 
