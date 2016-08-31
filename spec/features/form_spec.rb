852f70601255878bbc1438a5639e1c3b90c0ea61
Matthew Perkins <mfperkins@gmail.com>
2016-08-31 16:12:46 +0100

    21) Adding web_helpers ....

--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
   feature 'enter player names' do
     before :each do
-      visit('/')
-      fill_in :player_1_name, with: 'Dave'
-      fill_in :player_2_name, with: 'Mittens'
-      click_button 'submit'
+      sign_in_and_play
     end
     scenario 'submitting names' do
       expect(page).to have_content 'Dave vs. Mittens'
