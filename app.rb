f8413c60d8eb9ffb82659836774f749dcc03a462
Ewan Sheldon <ewansheldon@gmail.com>
2016-09-01 13:09:35 +0100

    attack takes points from opponent

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require 'player'
+require './lib/player'
 
 class Battle < Sinatra::Base
 
@@ -12,23 +12,22 @@ enable :sessions
   post '/names' do
     $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
-    session[:player_1_points] = 0
-    session[:player_2_points] = 0
     redirect '/play'
   end
 
   get '/play' do
     @player_1_name = $player_1.name
     @player_2_name = $player_2.name
-    @player_1_points = session[:player_1_points]
-    @player_2_points = session[:player_2_points]
+    @player_1_points = $player_1.hit_points
+    @player_2_points = $player_2.hit_points
     erb(:play)
   end
 
-  get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    erb(:attack)
+  post '/attack' do
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
+    redirect '/play'
   end
 
   # start the server if ruby file executed directly
