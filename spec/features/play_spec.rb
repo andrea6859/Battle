31aa796b989693206bc708b615a0f24e19558426
Ewan Sheldon <ewansheldon@gmail.com>
2016-09-01 15:58:09 +0100

    refactored to game class

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -4,12 +4,13 @@ feature 'attack player' do
   scenario 'click attack button' do
     sign_in_and_play
     click_button('Attack!')
-    expect(page).to have_content 'Dave: 60HP vs. Mittens: 50HP'
+    expect(page).to have_content 'Dave attacked Mittens'
   end
 
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack!'
+    click_link 'OK'
     expect(page).not_to have_content 'Mittens: 60HP'
     expect(page).to have_content 'Mittens: 50HP'
   end
